aeeb4b08a1c9ebbf359b09e8d74bc4d457c28784
abid982 <abidarif982@gmail.com>
2023-02-28 15:17:23 +0500

    showing error pages with errorElement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import RootLayout from './pages/Root';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ErrorPage from './pages/Error';
 
 // const routerDefinitions = createRoutesFromElements(
 //   <Route>
@@ -28,8 +29,11 @@ const router = createBrowserRouter([
     path: '/',
     // Parent route. It acts as a wrapper
     // In RootLayout we also have to define where these child route components and elements so the homepage and the products page should be rendered.
-    // Note: For complex pages you could have multiple root routes, for example one for slash admin, which then has a totally different set of children which might have a different layout that's wrapped around them.
+    // Note: For complex pages you could have multiple root routes, for example one for /admin, which then has a totally different set of children which might have a different layout that's wrapped around them.
     element: <RootLayout />,
+    // Fallback page if an error occurs
+    // The error is generated automatically by the react-router-dom package if we try to visit a page that doesn't exist.
+    errorElement: <ErrorPage />,
     // Children
     children: [
       { path: '/', element: <Home /> },
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,8 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from './../components/MainNavigation';
 
-import classes from './Root.module.css';
-
 // The Outlet Component marks the place where the child route elements should be rendered to.
 const RootLayout = () => {
   //   return <h1>Root Layout</h1>;
@@ -10,8 +8,8 @@ const RootLayout = () => {
     <>
       {/* <h1>Root Layout</h1> */}
       <MainNavigation />
-      {/* So this is now the marker the place wehre the child routes hould be rendered to. */}
-      <main className={classes.content}>
+      {/* So this is now the marker the place where the child routes held be rendered to. */}
+      <main>
         <Outlet />
       </main>
     </>
