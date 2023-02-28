f08a08797b67df6e72c56b1e0e877ca7d5c68466
abid982 <abidarif982@gmail.com>
2023-02-28 12:44:40 +0500

    navigating between pages and links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        {/* Go to <a href="/products">the list of products</a> */}
+        {/* Specify path we wanna go to */}
+        {/* Now what the link component does under the hood is it does render an anchor element but it basically listends for clicks on that element, prevents the browser default of sending an HTTP request if the link is clicked and instead takes a look at the route definitions to update the page accordingly and load the appropriate content. It also changes the URL but without sending a new HTTP request. */}
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
+  );
 };
 
 export default Home;
